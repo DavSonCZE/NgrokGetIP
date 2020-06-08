808224fecf7197482513c0e4040b0cbffa3b8261
DavSonCZE <40635601+DavSonCZE@users.noreply.github.com>
2020-06-08 20:28:39 +0200

    Update getIPFromAPP.bat

--- getIPFromAPP.bat
+++ getIPFromAPP.bat
@@ -4,8 +4,10 @@ COLOR 0B
 
 set cmd_curl=curl http://localhost:4040/api/tunnels
 set cmd_pid=tasklist /FI "imagename eq ngrok.exe"
-set cmd_del_file0=del /f output.txt"
-set cmd_copy=COPY "C:\Program Files\Ngrok\output\output.txt" "H:\XAMPP\htdocs\davson\publicip.html"
+set cmd_del_file0=del /f output.txt
+set cmd_del_file1=del /f output.txt
+set cmd_copy=COPY "C:\Program Files\Ngrok\output\output.txt" "H:\XAMPP\htdocs\davson\ip_data.html"
+set cmd_copy1=COPY "C:\Program Files\Ngrok\output\generated.txt" "H:\XAMPP\htdocs\davson\generated_time.html"
 
 ECHO  Ngrok - GetIPFromAPP
 ECHO ======================
@@ -18,7 +20,8 @@ ECHO -This tool still early access and can be broken.
 ECHO -LOG: %date% %time% in folder %cd% via %username%
 @echo:
 ::init things
-%md_del_file0%
+::%cmd_del_file0% //not working 
+::%cmd_del_file1% //not working
 @echo:
 ::Verification if running Ngrok
 ECHO Running Ngrok ? (I didn't see anything. Start NGROK!)
@@ -29,7 +32,9 @@ ECHO I'm getting data via Ngrok API.
 %cmd_curl%
 @echo:
 ::Save to the folder
+ECHO Generated information: %date% %time% > generated.txt
 %cmd_curl% > output.txt
 ::Copy to apache website
 %cmd_copy%
+%cmd_copy1%
 PAUSE
